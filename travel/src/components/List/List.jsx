e7cf6168e0fba9aa5978f468f35b0a2213306a15
Anna Kurmanova <annakurmanova0508@gmail.com>
2022-04-21 16:05:24 +0200

    fix

--- travel/src/components/List/List.jsx
+++ travel/src/components/List/List.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { CircularProgress, } from '@material-ui/core';
 
 const List = () => {
     return (
--- travel/src/components/Map/Map.jsx
+++ travel/src/components/Map/Map.jsx
@@ -4,29 +4,57 @@ import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab/Rating';
 
-import useStyles from './styles';
 
+import useStyles from './styles.js';
 
-const Map = () => {
+const Map = ({ coords, places, setCoords, setBounds, setChildClicked, weatherData }) => {
+    const matches = useMediaQuery('(min-width:600px)');
     const classes = useStyles();
-    // going to be set to false if larger than 600px
-    const isMobile = useMediaQuery('(min-width:600px)');
-    const coordinates = { lat: 0, lang: 0 };
+
     return (
         <div className={classes.mapContainer}>
-            <googleMapReact
-                bootstrapURLKeys={{ key: 'AIzaSyBwPvYpXbDZp2E_2xDTXtuZIqws0Vy9Ugw' }}
-                defaultCenter={coordinates}
-                center={coordinates}
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+                defaultCenter={coords}
+                center={coords}
                 defaultZoom={14}
                 margin={[50, 50, 50, 50]}
-                options={''}
-                onChange={''}
-                onChildClick={''}
+                options={{ disableDefaultUI: true, zoomControl: true }}
+                onChange={(e) => {
+                    setCoords({ lat: e.center.lat, lng: e.center.lng });
+                    setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+                }}
+                onChildClick={(child) => setChildClicked(child)}
             >
-            </googleMapReact>
+                {places.length && places.map((place, i) => (
+                    <div
+                        className={classes.markerContainer}
+                        lat={Number(place.latitude)}
+                        lng={Number(place.longitude)}
+                        key={i}
+                    >
+                        {!matches
+                            ? <LocationOnOutlinedIcon color="primary" fontSize="large" />
+                            : (
+                                <Paper elevation={3} className={classes.paper}>
+                                    <Typography className={classes.typography} variant="subtitle2" gutterBottom> {place.name}</Typography>
+                                    <img
+                                        className={classes.pointer}
+                                        src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                                    />
+                                    <Rating name="read-only" size="small" value={Number(place.rating)} readOnly />
+                                </Paper>
+                            )}
+                    </div>
+                ))}
+                {weatherData?.list?.length && weatherData.list.map((data, i) => (
+                    <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
+                        <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} height="70px" />
+                    </div>
+                ))}
+            </GoogleMapReact>
         </div>
-    )
-}
+    );
+};
 
 export default Map;
