f2c7c6271f6bc77e0fdf72751fc4758238de5dd5
Anna Kurmanova <annakurmanova0508@gmail.com>
2022-04-21 15:47:08 +0200

    added map

--- travel/src/components/Map/Map.jsx
+++ travel/src/components/Map/Map.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
+
+import useStyles from './styles';
+
 
 const Map = () => {
+    const classes = useStyles();
+    // going to be set to false if larger than 600px
+    const isMobile = useMediaQuery('(min-width:600px)');
+    const coordinates = { lat: 0, lang: 0 };
     return (
-        <div>Map</div>
+        <div className={classes.mapContainer}>
+            <googleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyBwPvYpXbDZp2E_2xDTXtuZIqws0Vy9Ugw' }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+            </googleMapReact>
+        </div>
     )
 }
 
