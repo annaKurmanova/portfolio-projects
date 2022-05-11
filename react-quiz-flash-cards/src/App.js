96fe351408b35fc64adc4430183c309ed69db47c
Anna Kurmanova <annakurmanova0508@gmail.com>
2022-05-11 12:17:56 +0200

    resize useEffect

--- react-quiz-flash-cards/src/App.js
+++ react-quiz-flash-cards/src/App.js
@@ -1,13 +1,37 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import FlashCardList from './components/FlashCardList';
+import axios from 'axios';
 
 function App() {
   const[flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+
+  useEffect(() => {
+    axios
+      .get('https://opentdb.com/api.php?amount=10')
+      .then(res => {
+       setFlashcards(res.data.results.map((questionItem, index) => {
+         const answer = decodeString(questionItem.correct_answer)
+         const options = [
+           ...questionItem.incorrect_answers.map(a => decodeString(a) ), answer]
+         return {
+           id: `${index}-${Date.now()}`,
+           question: decodeString(questionItem.question),
+           answer: answer ,
+           options: options.sort(() => Math.random() - .5)
+         }
+       }))
+      })
+  },[])
+
+  // fix encoding
+  function decodeString(string) {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = string
+    return textArea.value
+  }
   return (
-    
     <FlashCardList flashcards={flashcards}/>
-    
   );
 }
 
